dd1f4d557ee0f15ac2e77c12ec7e451ef87bdaa9
jagadeeshpuvvula <jagadeesh.puvvula@gmail.com>
2021-02-11 11:27:09 -0600

    Update DLNM_Heatwave.r

--- DLNM_Heatwave.r
+++ DLNM_Heatwave.r
@@ -1,65 +1,62 @@
-#LOAD LIBS
 library(dlnm)
 library(splines)
-
-#LOAD DATA
-coastal <- read.csv ("C:/Users/jagad/Desktop/NC_Sur/NC_Dec2019/coastal_dec.csv", header = T)
-
-# DATA FORMATTING
-cat <- c("tavg_95_2_coas","tavg_98_2_coas","tavg_95_3_coas","Tavg_98_3_coas","tavg_90_2_coas",
-         "tavg_90_3_coas","tmax_98_3_coas","tmax_98_2_coas","tmax_90_2_coas","tmax_90_3_coas",
-         "tmax_95_2_coas","tmax_95_3_coas","tmin_95_3_coas","tmin_98_2_coas","Tmin_98_3_coas",
-         "tmin_90_2_coas","tmin_90_3_coas","tmin_95_2_coas", "dow", "wDay")
-coastal[cat] <- lapply(coastal[cat], factor)
-
-coastal$ADMIT_SUP <- as.numeric(coastal$ADMIT_SUP)
-coastal$tmax <- as.numeric(coastal$tmax)
-coastal$tmin <- as.numeric(coastal$tmin)
-coastal$tavg <- as.numeric(coastal$tavg)
-coastal$date <- as.Date(coastal$date, format = "%m/%d/%Y")
-coastal$dow <- wday(as.Date(coastal$date, format = "%m/%d/%Y"))
-
+library(lubridate)
+library(mgcv)
+#=================================================================================
+#coastal
+dat <- read.csv ("C:/Users/jagad/Desktop/NC_Sur/NC_Dec2019/dat_octf.csv", header = T,
+                 fileEncoding="UTF-8-BOM")
+#piedmont
+dat <- read.csv ("C:/Users/jagad/Desktop/NC_Sur/NC_Dec2019/pied_octf.csv", header = T,
+                 fileEncoding="UTF-8-BOM")
+dat$date <- dat$Date # only for piedmont
+# ==================================================================================
+dat$date <- as.Date(dat$date, format = "%m/%d/%Y")
+dat$dow <- wday(as.Date(dat$date, format = "%m/%d/%Y"))
+dat$month<- as.factor(month(dat$date))
+dat$year<- as.factor(format(dat$date, '%Y'))
 weekdays1 <- c('Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday')
-coastal$wDay <- factor((weekdays(coastal$date) %in% weekdays1), 
+dat$wDay <- factor((weekdays(dat$date) %in% weekdays1), 
                        levels=c(FALSE, TRUE), labels=c('weekend', 'weekday'))
 
-coastal$doy <- as.numeric(format(coastal$date, "%d"))
-coastal$month <- as.numeric(format(coastal$date, "%m"))
-coastal$year <- as.numeric(format(coastal$date, "%Y"))
-coastal$dif <- as.numeric(coastal$tmax-coastal$tmin)
+cat <- c("month", "dow", "wDay", "year")
+dat[cat] <- lapply(dat[cat], factor)
+dat$Rate_ER_visit <- as.numeric(dat$imp_rate)
+dat$Count_ER_visit <- as.numeric(dat$imp_count)
+dat$Max_temp <- as.numeric(dat$tmax)
+# ==================================================================================
+varknots <- equalknots(dat$tmax,fun="bs",df=5,degree=2)
+lagknots <- logknots(5, 1)
+cb3.temp <- crossbasis(dat$Max_temp, lag=5, 
+                       argvar=list(fun="bs",knots=varknots), arglag=list(knots=lagknots))
+model3 <- gam(Count_ER_visit ~ cb3.temp+dow+month+year,
+              family=quasipoisson(), dat)
+# ==================================================================================
+#how to set the cen value
+pred3.temp <- crosspred(cb3.temp, model3,coef=NULL, vcov = NULL, cen=90, by=1, 
+                        from = 64, to = 98)
 
-# DLNM model implementation
-varknots <- equalknots(coastal$tmax,fun="bs",df=5,degree=2)
-lagknots <- logknots(30, 3)
-cb3.temp <- crossbasis(coastal$tmax, lag=30, argvar=list(fun="bs",
-                                                         knots=varknots),
-                       arglag=list(knots=lagknots))
+#Print risk ratio matrix
+print(pred3.temp$allRRfit)
 
-# GLM object
-model3 <- glm(ADMIT_SUP ~ tmax,
-              family=quasipoisson(), coastal)
-
-#wrap in DLNM framework
-pred3.temp <- crosspred(cb3.temp, model3, cen=60, by=1)
-
-#plot results
+#3d plot - mostly useless
 plot(pred3.temp, xlab="Temperature", zlab="RR", theta=200, phi=40, lphi=30,
      main="3D graph of temperature effect")
-####################################################################3
-#####################################################################
 
-#DLNM example 2
-library(lubridate)
-coastal$year <- year(as.Date(coastal$date, format = "%m/%d/%Y"))
+#relative risk | Temperature | lag
+plot(pred3.temp, "contour", xlab="Temperature", key.title=title("RR"),
+     plot.title=title("Contour plot",xlab="Temperature",ylab="Lag"))
 
-#Cross basis function
-cb2.temp <- crossbasis(coastal$tmax, lag=10,
-                       argvar=list(fun="thr",thr=c(60,77)), 
-                       arglag=list(fun="strata",breaks=c(2,6)),
-                       group=coastal$year)
-#GLM object
-model2 <- glm(ADMIT_SUP ~ tmax,
-              family=quasipoisson(), coastal)
-pred2.o3 <- crosspred(cb2.temp, model2, at=c(60:100))
+#Relative risk and lag days
+plot(pred3.temp, "slices", var=95, ci="n", col=1, ylim=c(0.95,1.25), lwd=1.5,
+     main="Lag-response curves")
 
-#Ref: https://cran.r-project.org/web/packages/dlnm/vignettes/dlnmTS.pdf
+#what happens at different lags and temp values [change var=c() for temp | lag=c() for lag days] 
+plot(pred3.temp, "slices", var=c(85,95), lag=c(0,5), col=4,
+     ci.arg=list(density=40,col=grey(0.7)))
+
+#what happens when exposed to 95 deg F (change var for change in temp)
+plot(pred3.temp, "slices", var=95, ci="bars", type="p", col=2, pch=19,
+     ci.level=0.95, main="Lag-response a 10-unit increase above threshold (95CI)")
+
+# ==================================================================================
